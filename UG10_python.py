4ac5cbd97a1f8c3496bda89317b130f607055ac8
Ivan <ivan.roberto@ti.ukdw.ac.id>
2024-11-13 00:17:56 +0700

    71230986

---
+++ UG10_python.py
@@ -0,0 +1,52 @@
+def konser(Tiket, Lokasi, TotalWaktu: int):
+    if Tiket == 'A':
+        hargaTiket = 350000
+    elif Tiket == 'B':
+        hargaTiket = 270000
+    else:
+        hargaTiket = 150000
+    
+    if Lokasi == 'Jakarta':
+        hargaTransport = 400000*2
+        hargaPenginapan = 0
+        if TotalWaktu > 24:
+            while TotalWaktu > 24:
+                hargaPenginapan += 250000 
+                TotalWaktu -= 24
+            hargaPenginapan += (TotalWaktu*40000)
+        elif TotalWaktu < 24:
+            hargaPenginapan += (TotalWaktu*40000)
+        else:
+            hargaPenginapan += 250000 
+
+    elif Lokasi == 'Bandung': 
+        hargaTransport = 390000*2   
+        hargaPenginapan = 0
+        if TotalWaktu > 24:
+            while TotalWaktu > 24:
+                hargaPenginapan += 200000 
+                TotalWaktu -= 24
+            hargaPenginapan += (TotalWaktu*30000)
+        elif TotalWaktu < 24:
+            hargaPenginapan += (TotalWaktu*30000)
+        else:
+            hargaPenginapan += 200000 
+        
+    elif Lokasi == 'Surabaya':
+        hargaTransport = 360000*2   
+        hargaPenginapan = 0
+        if TotalWaktu > 24:
+            while TotalWaktu > 24:
+                hargaPenginapan += 220000 
+                TotalWaktu -= 24
+            hargaPenginapan += (TotalWaktu*35000)
+        elif TotalWaktu < 24:
+            hargaPenginapan += (TotalWaktu*35000)
+        else:
+            hargaPenginapan += 360000 
+        
+    elif Lokasi == 'Yogyakarta':
+        hargaTransport = 0
+        hargaPenginapan = 0
+
+    return hargaTiket + hargaTransport + hargaPenginapan
---
+++ test.py
@@ -0,0 +1,6 @@
+from UG10_python import konser
+
+print(f"Test Case 1: {konser('A', 'Jakarta', 36) == 1880000}") #nilai 25
+print(f"Test Case 2: {konser('B', 'Surabaya', 11) == 1375000}") #nilai 25
+print(f"Test Case 3: {konser('C', 'Bandung', 24) == 1130000}") #nilai 25
+print(f"Test Case 4: {konser('A', 'Yogyakarta', 0) == 350000}") #nilai 25
